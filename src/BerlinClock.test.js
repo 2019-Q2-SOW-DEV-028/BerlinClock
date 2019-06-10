108d134b69ca54d9fdb08b7c3f7ef9ce0f3dd3c2
2019-Q2-SOW-DEV-028 <2019.q2.sow.dev.028@gmail.com>
2019-06-11 01:01:34 +0200

    test: Extracted common code to beforeEach method

--- src/BerlinClock.test.js
+++ src/BerlinClock.test.js
@@ -11,24 +11,25 @@ describe('Berlin clock', function(){
   let wrapper;
   let clock;
 
+  beforeEach(function(){
+    wrapper = shallow(<BerlinClock />);
+    clock = wrapper.instance();
+  });
+
   it('renders without crashing', () => {
     shallow(<BerlinClock />);
   });
 
   it('should not accept time if it is not valid', () => {
     let time = '29:00:85';
-    wrapper = shallow(<BerlinClock />);
-    clock = wrapper.instance();
 
     expect(clock.updateClock(time)).toBe('Invalid time');
   });
 
   it('should accept and set the time if it is a valid time', () => {
     let time = '22:10:25';
-    wrapper = shallow(<BerlinClock />);
-    clock = wrapper.instance();
     const spy = jest.spyOn(BerlinClock.prototype, 'setTime');
-    
+
     clock.updateClock(time);
 
     expect(spy).toHaveBeenCalled()
